03a899201a4049577ac8a2ef765efc7903f3d0b2
Christina D'Astolfo <chdastolfo@gmail.com>
2016-07-01 14:55:22 -0400

    initial commit, which cheese gem

---
+++ lib/which_cheese.rb
@@ -0,0 +1,16 @@
+require "which_cheese/version"
+
+module WhichCheese
+  require 'csv'
+
+	puts "**************"
+	puts "Hi! Here's what adult beverage you should drink and which cheese varieties you should pair it with.\n"
+	puts "**************"
+
+	def arr_of_cheeses 
+		chz = CSV.read("/applications/mamp/htdocs/rampup/cheeses.csv")
+		puts chz[rand(chz.length)]
+	end
+
+	arr_of_cheeses()
+end
---
+++ spec/which_cheese_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe WhichCheese do
+  it 'has a version number' do
+    expect(WhichCheese::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
